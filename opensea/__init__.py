d30c523b8b0d66b6cf4b305d7911d105720b1d7a
Attila Toth <hello@attilatoth.dev>
2021-11-07 08:21:59 +0100

    Sort out top level imports

--- opensea/__init__.py
+++ opensea/__init__.py
@@ -1,5 +1,10 @@
 """Top-level package for OpenSea API Python wrapper."""
 
-__author__ = """Attila Tóth"""
+__author__ = """Attila Toth"""
 __email__ = "hello@attilatoth.dev"
 __version__ = "__version__ = '0.1.1'"
+__all__ = ["Events", "Asset", "Assets", "Contract", "Collection",
+           "CollectionStats", "Collections", "Bundles"]
+
+from opensea.opensea import Events, Asset, Assets, Contract, Collection, \
+    CollectionStats, Collections, Bundles

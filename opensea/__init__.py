7a59255a1aac8420a3056a0e38ae86b69d4bae48
Attila Toth <hello@attilatoth.dev>
2021-11-20 00:21:03 +0100

    Add simplified module that supports api keys

--- opensea/__init__.py
+++ opensea/__init__.py
@@ -4,9 +4,10 @@ __author__ = """Attila Toth"""
 __email__ = "hello@attilatoth.dev"
 __version__ = "0.1.2"
 __all__ = ["Events", "Asset", "Assets", "Contract", "Collection",
-           "CollectionStats", "Collections", "Bundles", "utils"]
+           "CollectionStats", "Collections", "Bundles", "utils",
+           "OpenseaAPI"]
 
 from opensea.opensea import Events, Asset, Assets, Contract, Collection, \
     CollectionStats, Collections, Bundles
-
+from opensea.opensea_api import OpenseaAPI
 from opensea import utils

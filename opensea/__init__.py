f3b09b983f45ae87a2d4b0c998763057f52bb820
Attila Toth <hello@attilatoth.dev>
2021-11-19 14:38:07 +0100

    utils module

--- opensea/__init__.py
+++ opensea/__init__.py
@@ -4,7 +4,9 @@ __author__ = """Attila Toth"""
 __email__ = "hello@attilatoth.dev"
 __version__ = "0.1.2"
 __all__ = ["Events", "Asset", "Assets", "Contract", "Collection",
-           "CollectionStats", "Collections", "Bundles"]
+           "CollectionStats", "Collections", "Bundles", "utils"]
 
 from opensea.opensea import Events, Asset, Assets, Contract, Collection, \
     CollectionStats, Collections, Bundles
+
+from opensea import utils

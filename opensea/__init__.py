a6fb46889a1a3fb07274bebc0ffb455187d73387
Attila Toth <hello@attilatoth.dev>
2022-02-17 18:08:56 +0100

    Bump version: 0.1.4 → 0.1.5

--- opensea/__init__.py
+++ opensea/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Attila Toth"""
 __email__ = "hello@attilatoth.dev"
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __all__ = ["Events", "Asset", "Assets", "Contract", "Collection",
            "CollectionStats", "Collections", "Bundles", "utils",
            "OpenseaAPI"]
